c18848d270bde1398cbe0fedf7c969f38a0b573a
anqatech <jalal@anqatech.com>
2021-12-25 18:38:10 +0000

    Added list of users

--- src/FavoritedMovieList.js
+++ src/FavoritedMovieList.js
@@ -3,12 +3,19 @@ import React, { Component } from 'react';
 
 class FavoritedMovieList extends Component {
   render() {
+    const profiles = this.props.filteredProfiles
+
     return (
       <div>
-        <h2>Forrest Gump</h2>
+        <h2>{ this.props.moviename }</h2>
         <p>Liked By:</p>
         <ul>
-          <li>Nicholas Lain</li>
+          {profiles.map((profile) => {
+              const userObject = this.props.users[profile.userID]
+              return (
+                <li key={ this.props.movieid + profile.id }>{ userObject.name }</li>
+              )
+          })}
         </ul>
       </div>
     )
